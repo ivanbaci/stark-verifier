1cd1efb42e784f2d8c000cd756a20b3a2a5e7be0
Ivan Bacigaluppo <ivanbac1896@gmail.com>
2024-07-31 16:58:58 -0300

    feat: :sparkles: Add functions to set merkle and cps roots

--- contracts/StarkVerifier.sol
+++ contracts/StarkVerifier.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.26;
 
 contract StarkVerifier {
+    bytes32 public merkleRoot;
+    bytes32[] public cpRoots;
+
+    function setMerkleRoot(bytes32 _merkleRoot) public {
+        merkleRoot = _merkleRoot;
+    }
     
+    function setCpRoots(bytes32[] memory _cpRoots) public {
+        require(_cpRoots.length == 11, "Debe proporcionar los roots para cp0, cp1, ..., cp10");
+        delete cpRoots;
+        for (uint256 i = 0; i < 11; i++) {
+            cpRoots.push(_cpRoots[i]);
+        }
+    }
 }
